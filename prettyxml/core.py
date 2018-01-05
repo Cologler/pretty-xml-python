a3adb8ca90f3a3f9e2434f25a205b2b271ca433e
Cologler <skyoflw@gmail.com>
2018-01-05 16:37:43 +0800

    refactor

--- prettyxml/core.py
+++ prettyxml/core.py
@@ -6,91 +6,76 @@
 #
 # ----------
 
-import colorama
-from colorama import Fore, Style, Back
+from colorama import Fore
 
+from prettyxml.printer import Printer
 import prettyxml.xml as xml
 
-colorama.init()
+@Printer.register(xml.Declaration)
+def print_declaration(printer: Printer, obj: xml.Declaration):
+    attribs = ' '.join(['{}="{}"'.format(attr.name, attr.value) for attr in obj.attrib])
+    text = '<?{} {}?>'.format('xml', attribs)
+    printer.lightcyan(text).endline()
 
-def lightcyan(text):
-    return colorama.Fore.LIGHTCYAN_EX + text + colorama.Fore.RESET
+
+@Printer.register(xml.DocType)
+def _walk_doctype(printer: Printer, obj: xml.DocType):
+    attribs = ' '.join([str(attr) for attr in obj.childs])
+    text = '<!DOCTYPE {}>'.format(attribs)
+    printer.lightcyan(text).endline()
+
+
+@Printer.register(xml.Element)
+def _walk_element(printer: Printer, obj: xml.Element):
+    wrap_width = 100
+
+    text = Fore.LIGHTGREEN_EX
+    text += '<{}'.format(obj.tag)
+    text += Fore.RESET
+    for attr in obj.attrib:
+        text += ' '
+        text += Fore.LIGHTYELLOW_EX + attr.name
+        text += Fore.WHITE + '="'
+        text += Fore.LIGHTWHITE_EX + attr.value
+        text += Fore.WHITE + '"' + Fore.RESET
+
+    items = obj.items
+
+    if items:
+        text += Fore.LIGHTGREEN_EX + '>' + Fore.RESET
+        if len(items) == 1 and isinstance(items[0], xml.TextNode):
+            text_node = items[0]
+            if len(text_node.value) < wrap_width:
+                text += text_node.value + Fore.LIGHTGREEN_EX + '</{}>'.format(obj.tag) + Fore.RESET
+                printer.write(text).endline()
+                return
+            else:
+                printer.write(text).endline()
+                with printer.indent() as subprinter:
+                    subprinter.visit(text_node)
+        else:
+            printer.write(text).endline()
+            with printer.indent() as subprinter:
+                for item in items:
+                    subprinter.visit(item)
+    else:
+        printer.write(text)
+        printer.lightgreen('/>').endline()
+        return
+
+    printer.lightgreen('</{}>'.format(obj.tag)).endline()
+
+
+@Printer.register(xml.TextNode)
+def _walk_textnode(printer: Printer, obj: xml.TextNode):
+    printer.write(obj.value).endline()
+
 
 class XmlPrettifier:
     def __init__(self, xml_text: str):
-        self._indent = 0
+        self._printer = Printer()
         self._xml_text = xml_text
 
-        self.color_tag_open = Fore.LIGHTGREEN_EX
-        self.color_tag_close = Fore.LIGHTGREEN_EX
-        self.wrap_width = 100
-
-    def _println(self, val):
-        dt = ' ' * 2 * self._indent + val
-        print(dt)
-
-    def _walk(self):
-        for node in xml.fromstring(self._xml_text):
-            if isinstance(node, xml.Declaration):
-                self._walk_declaration(node)
-            elif isinstance(node, xml.DocType):
-                self._walk_doctype(node)
-            else:
-                self._walk_element(node)
-
-    def _walk_declaration(self, obj: xml.Declaration):
-        attribs = ' '.join(['{}="{}"'.format(attr.name, attr.value) for attr in obj.attrib])
-        text = '<?{} {}?>'.format('xml', attribs)
-        self._println(lightcyan(text))
-
-    def _walk_doctype(self, obj: xml.DocType):
-        attribs = ' '.join([str(attr) for attr in obj.childs])
-        text = '<!DOCTYPE {}>'.format(attribs)
-        self._println(lightcyan(text))
-
-    def _walk_element(self, obj: xml.Element):
-        text = self.color_tag_open
-        text += '<{}'.format(obj.tag)
-        text += Fore.RESET
-
-        for attr in obj.attrib:
-            text += ' '
-            text += Fore.LIGHTYELLOW_EX + attr.name
-            text += Fore.WHITE + '="'
-            text += Fore.LIGHTWHITE_EX + attr.value
-            text += Fore.WHITE + '"' + Fore.RESET
-
-        items = obj.items
-
-        if items:
-            text += Fore.LIGHTGREEN_EX + '>' + Fore.RESET
-            if len(items) == 1 and isinstance(items[0], xml.TextNode):
-                text_node = items[0]
-                if len(text_node.value) < self.wrap_width:
-                    text += text_node.value + self.color_tag_close + '</{}>'.format(obj.tag) + Fore.RESET
-                    self._println(text)
-                    return
-                else:
-                    self._println(text)
-                    self._indent += 1
-                    self._walk_text_node(text_node)
-                    self._indent -= 1
-            else:
-                self._println(text)
-                self._indent += 1
-                for item in items:
-                    self._walk_element(item)
-                self._indent -= 1
-        else:
-            text += Fore.LIGHTGREEN_EX + '/>' + Fore.RESET
-            self._println(text)
-            return
-        self._println(self.color_tag_close + '</{}>'.format(obj.tag) + Fore.RESET)
-
-    def _walk_text_node(self, obj):
-        self._println(obj.value)
-
     def print(self):
-        self._walk()
-
-
+        for node in xml.fromstring(self._xml_text):
+            self._printer.visit(node)
---
+++ prettyxml/printer.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+import colorama
+
+colorama.init()
+
+class Printer:
+    _TYPED_HANDLERS = {}
+
+    @classmethod
+    def register(cls, typ):
+        def _wrap(func):
+            cls._TYPED_HANDLERS[typ] = func
+            return func
+        return _wrap
+
+    def __init__(self, **kwargs):
+        self._root_printer = kwargs.get('root_printer', self)
+        self._indent_level = kwargs.get('indent_level', 0)
+        self._indent = '  '
+        self._newline = True
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        pass
+
+    def visit(self, node):
+        self._TYPED_HANDLERS[type(node)](self, node)
+
+    def indent(self):
+        return Printer(
+            indent_level=self._indent_level + 1,
+            root_printer=self._root_printer
+        )
+
+    def _get_indent(self):
+        return self._indent * self._indent_level
+
+    def write(self, text):
+        if self._newline:
+            text = self._get_indent() + text
+            self._newline = False
+        print(text, end='')
+        return self
+
+    def endline(self):
+        print()
+        self._newline = True
+        return self
+
+    def black(self, text):
+        return self.write(colorama.Fore.BLACK + text + colorama.Fore.RESET)
+
+    def red(self, text):
+        return self.write(colorama.Fore.RED + text + colorama.Fore.RESET)
+
+    def green(self, text):
+        return self.write(colorama.Fore.GREEN + text + colorama.Fore.RESET)
+
+    def yellow(self, text):
+        return self.write(colorama.Fore.YELLOW + text + colorama.Fore.RESET)
+
+    def blue(self, text):
+        return self.write(colorama.Fore.BLUE + text + colorama.Fore.RESET)
+
+    def magenta(self, text):
+        return self.write(colorama.Fore.MAGENTA + text + colorama.Fore.RESET)
+
+    def cyan(self, text):
+        return self.write(colorama.Fore.CYAN + text + colorama.Fore.RESET)
+
+    def white(self, text):
+        return self.write(colorama.Fore.WHITE + text + colorama.Fore.RESET)
+
+    def lightblack(self, text):
+        return self.write(colorama.Fore.LIGHTBLACK_EX + text + colorama.Fore.RESET)
+
+    def lightred(self, text):
+        return self.write(colorama.Fore.LIGHTRED_EX + text + colorama.Fore.RESET)
+
+    def lightgreen(self, text):
+        return self.write(colorama.Fore.LIGHTGREEN_EX + text + colorama.Fore.RESET)
+
+    def lightyellow(self, text):
+        return self.write(colorama.Fore.LIGHTYELLOW_EX + text + colorama.Fore.RESET)
+
+    def lightblue(self, text):
+        return self.write(colorama.Fore.LIGHTBLUE_EX + text + colorama.Fore.RESET)
+
+    def lightmagenta(self, text):
+        return self.write(colorama.Fore.LIGHTMAGENTA_EX + text + colorama.Fore.RESET)
+
+    def lightcyan(self, text):
+        return self.write(colorama.Fore.LIGHTCYAN_EX + text + colorama.Fore.RESET)
+
+    def lightwhite(self, text):
+        return self.write(colorama.Fore.LIGHTWHITE_EX + text + colorama.Fore.RESET)
